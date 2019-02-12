41f6f984c7e9e356c883d1a3f7f3fa01a9bf2f76
Paris Pantoulas <pntls3@gmail.com>
2019-02-13 01:24:54 +0200

    Conversations_Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  
-  root to: 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-  get 'login', to: 'devise/sessions#new'
-end
-devise_scope :user do
-  get 'signup', to: 'devise/registrations#new'
-end
-
-resources :posts do
-  collection do
-    get 'hobby'
-    get 'study'
-    get 'team'
+    devise_for :users, :controllers => {:registrations => "registrations"}
+    
+    root to: 'pages#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
   end
-end
-namespace :private do 
-  resources :conversations, only: [:create] do
-    member do
-      post :close
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
     end
   end
-  resources :messages, only: [:index, :create]
-end
+  namespace :private do 
+    resources :conversations, only: [:create] do
+      member do
+        post :close
+        post :open
+      end
+    end
+    resources :messages, only: [:index, :create]
+  end
 end
