877dfd5a5e92efe8dbbb9a30b3ba9286a2b3dc7e
Paris Pantoulas <pntls3@gmail.com>
2019-02-13 03:03:24 +0200

    Create a ContactsController and define routes to its actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,28 +4,29 @@ Rails.application.routes.draw do
     root to: 'pages#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-  end
-  devise_scope :user do
-    get 'signup', to: 'devise/registrations#new'
-  end
+      get 'login', to: 'devise/sessions#new'
+    end
+    devise_scope :user do
+      get 'signup', to: 'devise/registrations#new'
+    end
 
-  resources :posts do
-    collection do
-      get 'hobby'
-      get 'study'
-      get 'team'
-    end
-  end
-  namespace :private do 
-    resources :conversations, only: [:create] do
-      member do
-        post :close
-      end
-      member do
-        post :open
+    resources :posts do
+      collection do
+        get 'hobby'
+        get 'study'
+        get 'team'
       end
     end
-    resources :messages, only: [:index, :create]
-  end
+    namespace :private do 
+      resources :conversations, only: [:create] do
+        member do
+          post :close
+        end
+        member do
+          post :open
+        end
+      end
+      resources :messages, only: [:index, :create]
+      resources :contacts, only: [:create, :update, :destroy]
+    end
 end
