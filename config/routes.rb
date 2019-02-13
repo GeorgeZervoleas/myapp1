435826d0969c4fcc7e885b401381cbb6047cf04e
Paris Pantoulas <pntls3@gmail.com>
2019-02-13 15:23:23 +0200

    Create a MessengersController and define routes to its actions

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,12 @@ Rails.application.routes.draw do
       resources :messages, only: [:index, :create]
     end
     
-
+    namespace :messengers do
+      get 'messenger', to: 'messengers#index'
+      get 'get_private_conversation', to: 'messengers#get_private_conversation'
+      get 'get_group_conversation', to: 'messengers#get_group_conversation'
+      get 'open_messenger', to: 'messengers#open_messenger'
+    end
 
     
 end
