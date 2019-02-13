6da1e243d72753125a5e5b11a5e849f851d942d2
Paris Pantoulas <pntls3@gmail.com>
2019-02-13 13:40:44 +0200

    Define specs for Group::Conversations and Messasges

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,18 @@ Rails.application.routes.draw do
       resources :messages, only: [:index, :create]
       resources :contacts, only: [:create, :update, :destroy]
     end
+
+    namespace :group do 
+      resources :conversations do
+        member do
+          post :close
+          post :open
+        end
+      end
+      resources :messages, only: [:index, :create]
+    end
+    
+
+
+    
 end
