a74763d4769cd49d0d67c257c6d8719a80b3d0a5
Paris Pantoulas <pntls3@gmail.com>
2019-02-14 18:48:36 +0200

    change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
     devise_for :users, :controllers => {:registrations => "registrations"}
     
-    root to: 'pages#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     devise_scope :user do
       get 'login', to: 'devise/sessions#new'
     end
+
     devise_scope :user do
       get 'signup', to: 'devise/registrations#new'
     end
+
+    root to: 'pages#index'
+    get 'messenger', to: 'messengers#index'
+    get 'get_private_conversation', to: 'messengers#get_private_conversation'
+    get 'get_group_conversation', to: 'messengers#get_group_conversation'
+    get 'open_messenger', to: 'messengers#open_messenger'
 
     resources :posts do
       collection do
@@ -17,17 +23,15 @@ Rails.application.routes.draw do
         get 'team'
       end
     end
+
     namespace :private do 
       resources :conversations, only: [:create] do
         member do
           post :close
-        end
-        member do
           post :open
         end
       end
       resources :messages, only: [:index, :create]
-      resources :contacts, only: [:create, :update, :destroy]
     end
 
     namespace :group do 
@@ -39,13 +43,7 @@ Rails.application.routes.draw do
       end
       resources :messages, only: [:index, :create]
     end
-    
-    namespace :messengers do
-      get 'messenger', to: 'messengers#index'
-      get 'get_private_conversation', to: 'messengers#get_private_conversation'
-      get 'get_group_conversation', to: 'messengers#get_group_conversation'
-      get 'open_messenger', to: 'messengers#open_messenger'
-    end
 
-    
+    resources :contacts, only: [:create, :update, :destroy]
+            
 end
